ea1a2117622d9d7fcefd689d1c20bdd965e2ca41
Corecs <100100.98@mail.ru>
2023-02-28 20:31:29 +0400

    Downgrade spring boot to 2.7.5

--- src/main/java/com/accounts/accountmanagementapp/model/User.java
+++ src/main/java/com/accounts/accountmanagementapp/model/User.java
@@ -1,10 +1,10 @@
 package com.accounts.accountmanagementapp.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
