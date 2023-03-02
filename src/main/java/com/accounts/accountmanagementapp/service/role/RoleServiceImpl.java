3d83184f6498092f5cb581237dadd6be757971e3
Corecs <100100.98@mail.ru>
2023-03-02 20:35:00 +0400

    Add RoleServiceImpl

--- src/main/java/com/accounts/accountmanagementapp/service/role/RoleServiceImpl.java
+++ src/main/java/com/accounts/accountmanagementapp/service/role/RoleServiceImpl.java
@@ -1,4 +1,22 @@
 package com.accounts.accountmanagementapp.service.role;
 
-public class RoleServiceImpl {
+import com.accounts.accountmanagementapp.model.Role;
+import com.accounts.accountmanagementapp.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private RoleRepository roleRepository;
+
+    @Override
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
 }
